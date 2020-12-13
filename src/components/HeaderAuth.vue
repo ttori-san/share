7f59768af22b9f4fb2e9a572feee5456cadb2780
ttori-san <akira0901tori@softbank.ne.jp>
2020-12-13 19:06:50 +0900

    share-commit

---
+++ src/components/HeaderAuth.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class ="header flex">
+     <div class ="left">
+      <img class ="logo" src ="../assets/logo copy.png" alt/>
+    </div>
+    <div class ="right flex">
+      <p @click="$router.push('/signup')">新規登録</p>
+      <p @click="router.push('/')">ログイン</p>
+    </div>
+  </div>
+</template>
+<style scoped>
+.right {
+  align-items: center;
+}
+.right p{
+  margin-right:20px;
+  cursor :pointer;
+}
+.header {
+  margin :20px;
+}
+.logo{
+  width : 150px;
+  cursor : pointer;
+}
+.flex{
+  display:flex;
+  justify-content: space-between;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div class="flex">
+   <div class ="left">
+     <!-- <div class ="left-padding">
+       <img class ="logo" src="../assets/home.png" />
+       <div class="flex icon-field">
+         <img class="icon" src="../assets/profile.png" />
+         <p>ホーム</p>
+       </div>
+       <div class="flex icon-field">
+         <img class="icon" src="../assets/logout.png" />
+         <p>ログアウト</p>
+       </div>
+     </div>
+     <hr />
+    <div class="share">
+      <p>シェア</p>
+      <textarea></textarea>
+      <div>
+        <button>シェアする</button>
+      </div>
+    </div> -->
+    <SideNavi />
+   </div>
+   <div class="right">
+     <div class="title">
+       <p>ホーム</p>
+     </div>
+     <!-- <div v-for="(value,index) in shares" :key="index">
+       <div class="message">
+         <div class="flex">
+           <p class="name">{{value.name}}</p>
+           <img class="icon" src="../assets/heart.png"/>
+           <p class="number">{{value.like.length}}</p>
+           <img class="icon" src="../assets/cross.png"/>
+           <img class="icon detail" src="../assets/detail.png"/>
+         </div>
+         <p class="text">{{value.share}}</p>
+       </div>
+     </div> -->
+     <Message />
+   </div>
+ </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
+import SideNavi from "../components/SideNavi";
+import Message from "../components/Message";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  components:{
+    SideNavi,
+    Message
   }
-}
+};
 </script>
+
+<style scoped>
+.left {
+  width: 22%;
+  height: 100vh;
+}
+.right {
+  width: 78%;
+  height: 100vh;
+}
+.flex {
+  display: flex;
+}
+.title {
+  border-bottom: 1px solid white;
+  border-left: 1px solid white;
+  padding: 15px;
+}
+.title p {
+  font-size: 20px;
+  font-weight: bold;
+}
+ </style>
